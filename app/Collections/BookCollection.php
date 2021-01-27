a7aa99d3ea698337accc2e225e082df6bd15a14e
Miril Terolli <terolli.miril@gmail.com>
2021-01-26 21:17:59 -0500

    some refactoring for google api

--- app/Collections/BookCollection.php
+++ app/Collections/BookCollection.php
@@ -4,14 +4,28 @@
 namespace App\Collections;
 
 
+use App\Entities\Book;
 use Illuminate\Support\Collection;
 
 class BookCollection extends Collection
 {
-    public function forFrontend()
+    /**
+     * @return BookCollection
+     */
+    public function forFrontend(): BookCollection
     {
-        $this->transform(function ($item){
-           //
+        return $this->transform(function ($book){
+            return [
+                'id' => "{$book['id']}.{$book['etag']}",
+                'title' => !empty($book['volumeInfo']['title']) ? $book['volumeInfo']['title'] : '',
+                'snipped' => !empty($book['searchInfo']['textSnippet']) ? $book['searchInfo']['textSnippet'] : '',
+                'authors' => !empty($book['volumeInfo']['authors']) ? $book['volumeInfo']['authors'] : [],
+                'publishedDate' => !empty($book['volumeInfo']['publishedDate']) ? $book['volumeInfo']['publishedDate'] : '',
+                'smallThumbnail' => !empty($book['volumeInfo']['imageLinks']) ? $book['volumeInfo']['imageLinks']['smallThumbnail'] : '',
+                'thumbnail' => !empty($book['volumeInfo']['imageLinks']) ? $book['volumeInfo']['imageLinks']['thumbnail'] : '',
+                'selected' => false,
+                'position' => 0
+            ];
         });
     }
 
